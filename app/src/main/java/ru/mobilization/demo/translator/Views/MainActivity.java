00d36ee4afcb0b9ac7418446eefe7399fbbe8dbd
zamonitorom <onexmetalcore@gmail.com>
2017-04-19 22:00:58 +0300

    fragment

--- app/src/main/java/ru/mobilization/demo/translator/Views/MainActivity.java
+++ app/src/main/java/ru/mobilization/demo/translator/Views/MainActivity.java
@@ -1,5 +1,6 @@
-package ru.mobilization.demo.translator;
+package ru.mobilization.demo.translator.Views;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,9 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import ru.mobilization.demo.translator.R;
+import ru.mobilization.demo.translator.Views.Fragments.TranslatorFragment;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    public FragmentManager fragmentManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_translate:
-//                    mTextMessage.setText(R.string.title_translate);
+                    fragmentManager.beginTransaction().replace(R.id.container, new TranslatorFragment()).commit();
                     return true;
                 case R.id.navigation_history:
 //                    mTextMessage.setText(R.string.title_history);
@@ -38,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        fragmentManager = getFragmentManager();
     }
 
 }
